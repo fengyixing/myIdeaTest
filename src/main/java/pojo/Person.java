043bb51a36dabf143a2e37638c437c54c2807b2f
admin <1286374206@qq.com>
2020-04-18 17:06:00 +0800

    增加构造方法

--- src/main/java/pojo/Person.java
+++ src/main/java/pojo/Person.java
@@ -9,4 +9,12 @@ package pojo;
 public class Person {
     private String name;
     private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
