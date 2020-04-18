9341f8dec2c97b55c01fbc498c52e731441b98b5
admin <1286374206@qq.com>
2020-04-18 16:48:35 +0800

    添加工程

---
+++ src/main/java/pojo/Person.java
@@ -0,0 +1,12 @@
+/**
+ * @FileName: Person
+ * @Author: ASUS
+ * @Date: 2020/4/18 16:31
+ * @Description:
+ */
+package pojo;
+
+public class Person {
+    private String name;
+    private int age;
+}
